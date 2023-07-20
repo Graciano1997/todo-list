8772e30c05966ba8447ac4836fefa8b31416a8ee
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-07-20 04:28:30 +0100

    setup webpack

---
+++ dist/index.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunktodo_list"] = self["webpackChunktodo_list"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
